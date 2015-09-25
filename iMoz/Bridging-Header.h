af316ddff902cb689868d996a92fb6b8a89345c4
George Andrews <gandrews7@cvtc.edu>
2015-09-24 19:25:04 -0500

    Removing iMoz

--- iMoz/Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Bridging-Header.h
-//  iMoz
-//
-//  Created by George Andrews on 5/13/15.
-//  Copyright (c) 2015 CVTC Mobile Developer Program. All rights reserved.
-//
-
-#import <CommonCrypto/CommonHMAC.h>
--- iMoz/iMoz/HMAC.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HMAC.swift
-//  iMoz
-//
-//  Created by George Andrews on 5/24/15.
-//  Copyright (c) 2015 CVTC Mobile Developer Program. All rights reserved.
-//
-
-import Foundation
